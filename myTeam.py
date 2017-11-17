71d012d9a07b3f922013977b2d0f960035a56bb5
Lorenzo Boyice <renzob.kc@gmail.com>
2017-11-16 19:10:16 -0800

    mappings of players: mine, taken, available according to NFL

--- myTeam.py
+++ myTeam.py
@@ -5,12 +5,14 @@ import urllib2
 import re
 
 import pickle
+
+import copy
 from bs4 import BeautifulSoup
 
 NFL_PROVIDER = 'nfl'
 NFL_BASE_URL = 'http://fantasy.nfl.com/league/6009432/'
-NFL_TEAM_URL = NFL_BASE_URL+'team/%d?statCategory=projectedStats'
-NFL_PLAYERS_URL = NFL_BASE_URL+'players?playerStatus=available&statCategory=projectedStats&offset=%d'
+NFL_TEAM_URL = NFL_BASE_URL + 'team/%d?statCategory=projectedStats'
+NFL_PLAYERS_URL = NFL_BASE_URL + 'players?playerStatus=available&statCategory=projectedStats&offset=%d'
 
 NUMBER_FIRE_PROVIDER = 'number fire'
 NUMBER_FIRE_BASE_URL = 'https://www.numberfire.com/nfl/fantasy/fantasy-football-projections'
@@ -18,9 +20,9 @@ NUMBER_FIRE_BASE_URL = 'https://www.numberfire.com/nfl/fantasy/fantasy-football-
 FANTASY_PROS_PROVIDER = 'fantasy pros'
 FANTASY_PROS_BASE_URL = 'https://www.fantasypros.com/nfl/projections/'
 
-MY_TEAM_FILE = 'players/my_team.html'
-TAKE_PLAYERS_FILE = 'players/taken_players_%d.html'
-AVAILABLE_PLAYERS_FILE = 'available_players.pkl'
+MY_TEAM_FILE = 'players/mine.html'
+TAKE_PLAYERS_FILE = 'players/taken/taken_page_%d.html'
+AVAILABLE_PLAYERS_FILE = 'players/available/available_page_%d.html'
 IR_PLAYERS_FILE = 'ir_players.json'
 
 
@@ -41,13 +43,11 @@ class Projections:
 
 
 class Player:
-
-    projections = dict()
-
     def __init__(self, pl_name, pl_position, pl_team):
         self.name = pl_name
         self.position = pl_position
         self.team = pl_team
+        self.projections = dict()
 
     def add_projections(self, week, provider, projection):
         try:
@@ -77,24 +77,30 @@ def is_table_row(tag):
     return tag.has_attr('data-row-index')
 
 
-def build_nfl_player_with_projections(_soup, _current_week, _map, mine=False):
+def build_nfl_player_with_projections(_soup, _current_week, _map, individual=False):
+    _not_playing = []
     if re.match("\d", _current_week) is None:
         raise Exception("Bad week")
-    match = re.compile("player-\d")
-    if mine:
-        match = re.compile("player-\d-\d")
-    for _row in _soup('tr', class_=match):
+    _current_week = int(_current_week)
+    match = re.compile("^player-\d+")
+    if individual:
+        match = re.compile("^player-\d+-\d+")
+    rows = _soup('tr', class_=match)
+    if len(rows) is not 15 and individual:
+        raise Exception("Bad matching for rows")
+    for _row in rows:
         player_name = _row.find("a", class_="playerName").string
         pattern = "\w+|\w\.\w\. \w+"
         if re.match(pattern, player_name) is None:
             raise Exception("Bad name: %s, wanted: %s" % (player_name, pattern))
 
         player_position, player_team = parse_position_team(_row.find("em").string)
-        pattern = "\w\w"
+        pattern = "\w\w|\w"
         if re.match(pattern, player_position) is None:
             raise Exception("Bad position: %s, wanted: %s" % (player_position, pattern))
         if re.match(pattern, player_team) is None:
             if "N/A" in player_team:
+                _not_playing.append(player_name)
                 # print "Free Agent: name-%s, position-%s" % (player_name, player_position)
                 continue
             raise Exception("Bad team: %s, wanted: %s" % (player_team, pattern))
@@ -102,23 +108,25 @@ def build_nfl_player_with_projections(_soup, _current_week, _map, mine=False):
         try:
             projected_points = _row.find("span", class_="playerWeekProjectedPts").string
         except AttributeError:
-            ir_players.append(player_name)
+            _not_playing.append(player_name)
             # print "IR player: name-%s, pos-%s, team-%s" % (player_name, player_position, player_team)
             continue
         pattern = "\d+.\d+"
         if re.match(pattern, projected_points) is None:
             if "-" in projected_points:
+                _not_playing.append(player_name)
                 # print "Out: name-%s, pos-%s, team-%s" % (player_name, player_position, player_team)
                 continue
             raise Exception("Bad points: %s, wanted: %s" % (projected_points, pattern))
-        if float(projected_points) <= 1.0:
+        if float(projected_points) <= 1.0 and not individual:
             break
 
         new_player = Player(player_name, player_position, player_team)
         new_player.add_projections(_current_week, NFL_PROVIDER, projected_points)
         _map[player_name] = new_player
         new_player, player_name, player_team, player_position, projected_points = None, None, None, None, None
-    return ir_players
+    # print len(_map)
+    return _not_playing
 
 
 try:
@@ -134,12 +142,12 @@ except IOError:
 soup = BeautifulSoup(html_doc, 'html.parser')
 my_team = {}
 current_week = soup.find("div", class_="stat-type-nav").find("li", class_="selected").find("span", class_="text").string
-if re.match("\d", current_week) is None:
+if re.match("\d+", current_week) is None:
     raise Exception("Bad week")
-ir_players = build_nfl_player_with_projections(soup, current_week, my_team, True)
+not_playing = build_nfl_player_with_projections(soup, current_week, my_team, True)
 len_ = len(my_team)
 if len_ is not 15:
-    print "ERROR: Fix your roster!"
+    print "Houston, Fix your roster!"
 print "My team has %d players!" % len_
 
 taken_players_files = []
@@ -156,34 +164,44 @@ except IOError:
         with open(TAKE_PLAYERS_FILE % i, 'w') as outfile:
             outfile.write(html_doc)
             outfile.close()
-taken_players = []
+taken_players = {}
 for i in range(0, 9):
     soup = BeautifulSoup(taken_players_files[i], 'html.parser')
-    for taken_player in soup(class_='playerName'):
-        taken_players.append(taken_player.string)
-print "There are %d taken players" % len(taken_players)
+    current_week = soup.find("div", class_="stat-type-nav").find("li", class_="selected").find("span",
+                                                                                               class_="text").string
+    result = build_nfl_player_with_projections(soup, current_week, taken_players, True)
+    for p in result:
+        not_playing.append(p)
+len_taken = len(taken_players)
+print "There are %d taken active players!" % len_taken
+if len_taken + len(not_playing) is not 135:
+    print "Houston, someone does not a have a full roster!"
 
-
+available_players_files = []
 try:
-    available_players = pickle.load(open(AVAILABLE_PLAYERS_FILE))
-    ir_players = json.load(open(IR_PLAYERS_FILE))
+    for i in range(0, 6):
+        with open(AVAILABLE_PLAYERS_FILE % i) as infile:
+            available_players_files.append(infile.read())
+            infile.close()
 except IOError:
-    print "Building available players and nfl projections..."
-    available_players = {}
-    ir_players = []
-    for i in range(0, 28):
-        offset = i*25+1
+    print "Retrieving available players pages..."
+    for i in range(0, 6):
+        offset = i * 25
         html_doc = urllib2.urlopen(NFL_PLAYERS_URL % offset).read()
-        soup = BeautifulSoup(html_doc, 'html.parser')
-        current_week = soup.find("div", class_="stat-type-nav").find("li", class_="selected").find("span",
-                                                                                                   class_="text").string
-        build_nfl_player_with_projections(soup, current_week, available_players)
-        with open(AVAILABLE_PLAYERS_FILE, 'w') as outfile:
-            pickle.dump(available_players, outfile)
-    with open(IR_PLAYERS_FILE, 'w') as output_file:
-        json.dump(ir_players, output_file)
+        available_players_files.append(html_doc)
+        with open(AVAILABLE_PLAYERS_FILE % i, 'w') as outfile:
+            outfile.write(html_doc)
+            outfile.close()
+available_players = {}
+for i in range(0, 6):
+    soup = BeautifulSoup(available_players_files[i], 'html.parser')
+    current_week = soup.find("div", class_="stat-type-nav").find("li", class_="selected").find("span",
+                                                                                               class_="text").string
+    result = build_nfl_player_with_projections(soup, current_week, available_players, False)
+    for p in result:
+        not_playing.append(p)
 map_length = len(available_players)
-print "There are %d available players! And %d injured soldiers." % (map_length, len(ir_players))
+print "There are %d available players!" % map_length
 
 html_doc = urllib2.urlopen(NUMBER_FIRE_BASE_URL).read()
 soup = BeautifulSoup(html_doc, 'html.parser')
@@ -198,17 +216,14 @@ for row in soup(is_table_row):
         nf_name = row.find("span", class_='full').string
         _buffer = nf_name.split(" ")
         nf_name = _buffer[0] + " " + _buffer[1]
-        try:
+        player = None
+        if nf_name in available_players:
             player = available_players[nf_name]
-            # print "Hit on %s!" % nf_name
-            continue
-        except KeyError:
-            if nf_name in taken_players:
-                continue
-            if nf_name in my_team:
-                continue
-            if nf_name in ir_players:
-                continue
+        if nf_name in taken_players:
+            player = taken_players[nf_name]
+        if nf_name in my_team:
+            player = my_team[nf_name]
+        if player is None:
             print "Unknown player %s. I should probably create him" % nf_name
     else:
         print row
@@ -218,4 +233,3 @@ for row in soup(is_table_row):
 # soup = BeautifulSoup(html_doc, 'html.parser')
 # for player in soup(class_='player-name'):
 #     print player.string
-
